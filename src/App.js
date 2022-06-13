1d6d612e743f6e436e5e4c86c994a040909cbd54
Vadim-Kirtsun <vadiakirtsun.com>
2022-06-13 20:02:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import './App.css';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      WORKING
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/utils/consts.js
@@ -0,0 +1,5 @@
+export const ADMIN_ROUTER = '/admin'
+export const LOGIN_ROUTER = '/login'
+export const REGISTRATION_ROUTER = '/registration'
+export const COLLECTIONS_ROUTER = '/collections'
+export const ITEMS_ROUTER = '/items'
