8bdb769677dcd2b57247b8309b4b4c11dfd23288
Vadim-Kirtsun <vadiakirtsun.com>
2022-06-17 19:12:51 +0300

    add NavBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import './App.css';
 import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar"
 
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRouter/>
+        <NavBar />
+        <AppRouter />
     </BrowserRouter>
   );
 };
